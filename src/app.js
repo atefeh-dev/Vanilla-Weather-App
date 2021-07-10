6984f48bbe7517b7e5cdeb41ef7f96a2c33dee07
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2021-07-10 20:53:45 +0430

    add locatin

--- src/app.js
+++ src/app.js
@@ -108,6 +108,20 @@ function search(city) {
   axios.get(apiUrl).then(displayTemperature);
 }
 
+let allowLoc = document.getElementById("#current");
+
+allowLoc.addEventListener("click", (e) => {
+  e.preventDefault();
+  navigator.geolocation.getCurrentPosition((position) => {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+    axios.get(url).then((response) => {
+      search(response.data.name);
+    });
+  });
+});
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
